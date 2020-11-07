61b03ba2b0656d06fbc2919e0b2bace1987ce37c
cf17jeremy <cf17jeremy.torres@iesjoandaustria.org>
2020-11-07 20:07:40 +0100

    update

--- app/src/main/java/com/example/gestorincidencies/menu.java
+++ app/src/main/java/com/example/gestorincidencies/menu.java
@@ -1,4 +1,23 @@
 package com.example.gestorincidencies;
 
-public class menu {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.nightonke.boommenu.BoomButtons.SimpleCircleButton;
+import com.nightonke.boommenu.BoomMenuButton;
+import com.nightonke.boommenu.ButtonEnum;
+
+public class menu extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_layout);
+        BoomMenuButton bmb = (BoomMenuButton) findViewById(R.id.bmb);
+        bmb.setButtonEnum(ButtonEnum.SimpleCircle);
+        for (int i = 0; i < bmb.getButtonPlaceEnum().buttonNumber(); i++) {
+            bmb.addBuilder(new SimpleCircleButton.Builder()
+                    .normalImageRes(R.drawable.plus));
+        }
+    }
 }
